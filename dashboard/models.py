3f169b0e1b1189517d43a0d16c6d9899fe0f0e11
Sidharth S <sidharth.s16@yahoo.com>
2021-05-27 18:32:03 +0100

    Added a simple diagnostics report generator

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,20 +4,13 @@ from wagtail.admin.edit_handlers import HelpPanel
 
 # Create your models here.
 
-@register_setting(icon='placeholder')
+@register_setting(icon='help')
 class AboutEngine(BaseSetting):
     panels = [
         HelpPanel(
             template='engine/index.html',
         )
     ]
-
-    def change_view(self, request, object_id, extra_context=None):
-        if not request.user.is_superuser:
-            extra_context = extra_context or {}
-            extra_context['readonly'] = True
-
-        return super(AboutEngine, self).change_view(request, object_id, extra_context=extra_context)
 
     class Meta:
         verbose_name = 'About Engine'
--- dashboard/templatetags/engine_dashboard_tags.py
+++ dashboard/templatetags/engine_dashboard_tags.py
@@ -1,9 +1,30 @@
 from django import template
 
 from engine import VERSION
+import pkg_resources
 
 register = template.Library()
 
 @register.simple_tag
 def get_engine_version():
     return VERSION
+
+@register.simple_tag
+def get_pip_requirements():
+    installed_packages = pkg_resources.working_set
+    installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+    for i in installed_packages])
+    return installed_packages_list
+
+@register.simple_tag
+def get_pip_list():
+    return [p.project_name + " @ " + str(p.parsed_version) for p in pkg_resources.working_set]
+
+@register.simple_tag
+def diagnostics_report():
+    report = {
+        "engine_version": get_engine_version(),
+        "requirements_txt": get_pip_requirements(),
+        "pip_list": get_pip_list(),
+    }
+    return report
