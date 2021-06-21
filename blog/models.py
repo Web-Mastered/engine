8eeeecb7697b2019a275b01fb0f7724e3c139d26
Sidharth S <sidharth.s16@yahoo.com>
2021-06-21 16:18:38 +0100

    Added more docstrings and comments

--- blog/models.py
+++ blog/models.py
@@ -30,6 +30,7 @@ class BlogListingPage(RoutablePageMixin, Page, BlogListingPageFields):
     content_panels = Page.content_panels + BlogListingPageFields.content_panels
 
     def get_context(self, request, *args, **kwargs):
+        """Pass a list of blog post pages as context"""
         context = super().get_context(request, *args, **kwargs)
         context["posts"] = BlogPostPage.objects.live().public().order_by('-first_published_at')
         return context
@@ -40,17 +41,20 @@ class BlogListingPage(RoutablePageMixin, Page, BlogListingPageFields):
 
     @route(r'^categories/$')
     def category_listing_page(self, request, *args, **kwargs):
+        """This handles the category listing pages"""
         context = self.get_context(request, *args, **kwargs)
         categorySlug = request.GET.get('category')
         if categorySlug not in (None,""):
             context = self.get_context(request, *args, **kwargs)
             category = BlogPostCategory.objects.filter(slug=categorySlug)
             if len(category) != 1:
+                """If no category model matches with the query..."""
                 context['blog_listing_page'] = BlogListingPage.objects.all()[0]
                 context['errorTitle'] = "Category not found"
                 context['errorMessage'] = "The category that you're looking for does not exist, please double check your query."
                 return render(request, "blog/error_page.html", context)
             else:
+                """If there is a match with a category model..."""
                 context['blog_listing_page'] = BlogListingPage.objects.all()[0]
                 context['posts'] = BlogPostPage.objects.filter(categories__in=category).live().public().order_by('-first_published_at')
                 context['category'] = category.first
@@ -81,12 +85,14 @@ class BlogPostPage(Page, BlogPostPageFields):
         verbose_name_plural = "Blog Posts"
 
     def get_absolute_url(self):
+        """Get the URL of this model (used for django-comments-xtd)"""
         if settings.DEBUG:
             return 'http://localhost:8000' + self.url
         else:
             return self.full_url
 
     def get_context(self, request, *args, **kwargs):
+        """Pass the blog listing page object as a context"""
         context = super().get_context(request, *args, **kwargs)
         context['blog_listing_page'] = BlogListingPage.objects.all()[0]
         return context
@@ -130,6 +136,7 @@ class BlogPostComment(XtdComment):
         verbose_name_plural = "Blog Post Comments"
 
     def save(self, *args, **kwargs):
+        """Function that defines what happens when a comment is published (saved)"""
         if self.user:
             self.user_name = self.user.username
         self.page = BlogPostPage.objects.get(pk=self.object_pk)
--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -9,12 +9,14 @@ subtitle = ""
 
 @register.simple_tag
 def set_featured_img_url(val=None):
+    """Allows featured image URL to be stored"""
     global featured_img_url
     featured_img_url = val
     return True
 
 @register.simple_tag
 def get_featured_img_url():
+    """Allows featured image URL to be retrieved"""
     global featured_img_url
     this_featured_img_url = featured_img_url
     featured_img_url = None
@@ -22,12 +24,14 @@ def get_featured_img_url():
 
 @register.simple_tag
 def set_title(val=None):
+    """Allows title text to be stored"""
     global title
     title = val
     return True
 
 @register.simple_tag
 def get_title():
+    """Allows title text to be retrieved"""
     global title
     this_title = title
     title = None
@@ -35,12 +39,14 @@ def get_title():
 
 @register.simple_tag
 def set_subtitle(val=None):
+    """Allows subtitle text to be stored"""
     global subtitle
     subtitle = val
     return True
 
 @register.simple_tag
 def get_subtitle():
+    """Allows subtitle text to be retrieved"""
     global subtitle
     this_subtitle = subtitle
     subtitle = None
@@ -48,5 +54,6 @@ def get_subtitle():
 
 @register.simple_tag
 def experimental_comments_status():
+    """Returns the global enable/disable status of the comments system"""
     commenting_status = settings.ENABLE_EXPERIMENTAL_BLOG_COMMENTING
     return commenting_status
