2c20002605ea666f346dbdbf771d423d393382c7
Sidharth S <sidharth.s16@yahoo.com>
2021-06-27 14:20:49 +0100

    Added filled template information

--- engine/__init__.py
+++ engine/__init__.py
@@ -17,10 +17,30 @@ $$$$$$$$\ $$ |  $$ |\$$$$$$$ |$$ |$$ |  $$ |\$$$$$$$\
 __title__ = 'Engine CMS'
 __version__ = '1.0.0-dev'
 __author__ = 'Sidharth Shanmugam'
-__company__ = 'Web Mastered'
-__license__ = ''            # Need to choose a license
-__copyright__ = ''          # Need to give it a copywrite
-__client__ = ''             # Name of the client
+__company__ = 'Web Mastered Ltd'
+__license__ = """
+Your continued use of our softwares implies your agreement to the terms and 
+conditions provided by Web Mastered Ltd, including, but not limited to the following
+mentioned below.
+
+THIS SOFTWARE IS PROVIDED BY WEB MASTERED LTD "AS IS" AND ANY EXPRESS OR 
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+SHALL WEB MASTERED LTD BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH 
+DAMAGE. WEB MASTERED LTD HOLDS THE RIGHT TO DISABLE OR DELETE WEBSITES AND DATA IN THE EVENT
+WHERE THE TERMS AND CONDITIONS HAVE BEEN BREACHED.
+
+Web Mastered Ltd will not be responsible for any forms of damages, including, but not limited 
+to data loss. Web Mastered Ltd reserve the right to terminate any warranties, agreements or
+subscriptions if the terms and conditions have been breached.
+"""
+__copyright__ = 'Copyright (c) 2021-present, Web Mastered Ltd. All rights reserved.'
+__client__ = ''
 
 TITLE = __title__
 VERSION = __version__
