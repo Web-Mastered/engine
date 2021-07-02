d7ecc1a7db9d0573d0cf27e7e0b3d114c62482b7
Sidharth S <sidharth.s16@yahoo.com>
2021-07-02 13:33:04 +0100

    Testing with COMPRESS_ENABLED

--- engine/settings/base.py
+++ engine/settings/base.py
@@ -191,6 +191,7 @@ COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
 
+COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 LIBSASS_OUTPUT_STYLE = 'compressed'
 LIBSASS_SOURCEMAPS = True
